45f3d9cfbb264cfee8e54be94914afb118b38e82
Adrian <adrianortegaolgoso@gmail.com>
2025-05-11 20:39:56 +0200

    fix: cambiado icono y nombre de app

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Binding of Isaac Items',
+      title: 'Free Games App',
       theme: ThemeData(
         primarySwatch: Colors.purple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
